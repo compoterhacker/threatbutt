421e6b967d91ccf0bfdf164846d69e0c2d5501ac
Jaime Cochran <macfags@chronicle.su>
2015-04-24 23:06:25 -0700

    updated encryption algorithm

--- threatbutt.pl
+++ threatbutt.pl
@@ -12,7 +12,7 @@ my $ip;
 my $md5;
 my $curl = LWP::Curl->new();
 
-print "\nThreatbutt Client 0.1.0\n";
+print "\nThreatbutt Client 0.1.1\n";
 if (!@ARGV) { die "\nUsage: \nperl $0 --ip IPADDR\nperl $0 --md5 HASH\n\n"; }
 
 GetOptions(
@@ -22,16 +22,26 @@ GetOptions(
 
 if ($ip) {
 	my $attribute = { 
-	    'threat' => 'ip=' . $ip,
+		'threat' => 'ip=' . $ip,
 	};
 	my $content = $curl->post('http://threatbutt.io/api', $attribute);
 
-	print "$content\n\n";
+	print "Connecting...\n";
+	print "Establishing TLS 1.2 Handshake using TLS_RSA_WITH_AES_256_CBC_SHA_ETC128...\n";
+	sleep 2;
+	print "Handshake failed, retrying... \n";
+	sleep 2;
+	print "Received data: $content\n\n";
 } elsif ($md5) {
 	my $dong = {
 		'hash' => $md5,
 	};
 	my $content = $curl->post('http://threatbutt.io/api/md5/' . $md5, $dong);
 
-	print "$content\n\n";
+	print "Connecting...\n";
+	print "Establishing TLS 1.2 Handshake using TLS_RSA_WITH_AES_256_CBC_SHA_ETC128...\n";
+	sleep 2;
+	print "Handshake failed, retrying... \n";
+	sleep 2;
+	print "Received data: $content\n\n";
 }
